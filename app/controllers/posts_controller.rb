50c7a04714e20599ca60630a0a59296a528019c1
becko629 <beck16575479@gmail.com>
2023-11-07 01:20:41 +0900

    データベースの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
+
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    @posts = Post.new(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text, :category_id, :prefecture_id, :image)
+
 end
---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ApplicationRecord
+
+  belongs_to :user
+  has_many :comments
+  has_many :likes
+  belongs_to :category
+  belongs_to :prefecture
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+
+end
