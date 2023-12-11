5ebec261a72c4239b68949d2f9ecd7562e36f4f7
becko629 <beck16575479@gmail.com>
2023-12-12 00:49:25 +0900

    テストコードの実行

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :comment do
-    
+    comment_text { Faker::Lorem.sentence }
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'コメント投稿' do
+  before do
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post)
+    @comment = FactoryBot.build(:comment, user_id: user.id, post_id: post.id)
+end
+
+  context 'コメント投稿ができるとき' do
+    it 'コメントがあれば登録できる' do
+      expect(@comment).to be_valid
+    end
+    it 'commentは、30文字以内であれば登録できる' do
+      expect(@comment).to be_valid
+    end
+  end
+
+  context 'コメントができない時' do
+    it 'commentがないとコメントできない' do
+      @comment.comment_text = ' '
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include include("Comment text can't be blank")
+    end
+    it 'commentは30文字以上ではコメントできない' do
+      @comment.comment_text = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include include("Comment text is too long (maximum is 30 characters)")
+    end
+  end
+end
 end
