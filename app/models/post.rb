fdf1458306a1f665b27046585df37a4398077c9d
becko629 <beck16575479@gmail.com>
2024-01-19 17:55:22 +0900

    モデル、いいねボタンの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,7 @@ class Post < ApplicationRecord
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
